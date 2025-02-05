a40ede3c04f8a9310455faed9f3f495c093a5f4b
JeanAriza <262419801+ja-ArsLongaVitaBrevis@users.noreply.github.com>
2025-02-05 06:28:57 +0100

    Add GCloud setup

---
+++ .gcloud.sh
@@ -0,0 +1,5 @@
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/jesuarva/google-cloud-sdk/path.bash.inc' ]; then . '/Users/jesuarva/google-cloud-sdk/path.bash.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/jesuarva/google-cloud-sdk/completion.bash.inc' ]; then . '/Users/jesuarva/google-cloud-sdk/completion.bash.inc'; fi
